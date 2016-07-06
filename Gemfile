7ba27d7cc18787cfa09668a8769a500cbfba8d39
leearaneta <lee.araneta@gmail.com>
2016-07-06 19:15:06 -0400

    changed routes and installed humanize

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
 
+gem 'humanize'
 gem 'geocoder'
 gem 'yelp', require: 'yelp'
 gem 'better_errors'
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   resources :reverse_locations
-  resources :meet_mes
+  resources :meet_mes, only: :create
+
+  get '/meetmehalfway' => 'meet_mes#new', as: 'new_meet_me'
+  get '/meetmehalfway/:id' => 'meet_mes#show', as: 'meet_me'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
