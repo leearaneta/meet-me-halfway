d0ccb140fffe621f92fc131057e5d6e0f29c0d72
yyangflatiron <yichen.yang@flatironschool.com>
2016-09-14 14:03:32 -0400

    heroku

--- app/models/meet_me.rb
+++ app/models/meet_me.rb
@@ -7,19 +7,23 @@ class MeetMe < ApplicationRecord
       address.name
     end
     Geocoder::Calculations.geographic_center(array)
+    #Geocoder takes in multiple addresses and calculates the midpoint in lat long
   end
 
   def find_address
     midpoint = self.find_midpoint
     ReverseLocation.create(latitude: midpoint[0], longitude: midpoint[1]).address
+    #Reverse geocoder takes in the midpoint's coordinates and calculates the actual address
   end
 
   def find_params
     params = {term: self.term, limit: self.results, sort: 2}
+    #returns the attributes of the meet_me object
   end
 
   def associate_addresses
     self.addresses.each { |address| address.meet_me = self }
+    #make address belong to meet_me
   end
 
   def yelp_query
@@ -29,6 +33,7 @@ class MeetMe < ApplicationRecord
       Yelp.client.search(address, params).businesses
     rescue
       self.addresses = []
+      #exceptions
     end
   end
 
@@ -36,10 +41,13 @@ class MeetMe < ApplicationRecord
     self.yelp_query
     if self.addresses.empty?
       return "you'll probably end up in the ocean"
+      #when midpoint doesnt have a valid address
     elsif self.yelp_query.count == 0
       return "There are no vendors of this category in your area."
+      #valid midpoint, but no vendors nearby
     elsif self.find_midpoint[0].nan?
       return "Please enter valid addresses."
+      #addresses are not in the correct format
     end
   end
 
